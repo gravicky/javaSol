38cb247927e8db66408325d2ea82499012950aea
gravicky <100748949+gravicky@users.noreply.github.com>
2022-03-03 20:52:04 +0530

    Time: 0 ms (100.00%), Space: 44.2 MB (8.67%) - LeetHub

--- 108-convert-sorted-array-to-binary-search-tree/108-convert-sorted-array-to-binary-search-tree.java
+++ 108-convert-sorted-array-to-binary-search-tree/108-convert-sorted-array-to-binary-search-tree.java
@@ -14,39 +14,17 @@
  * }
  */
 public class Solution {
-    
-    public TreeNode sortedArrayToBST(int[] nums) {
-        
-        int len = nums.length;
-        if ( len == 0 ) { return null; }
-        
-        // 0 as a placeholder
-        TreeNode head = new TreeNode(0); 
-        
-        Deque<TreeNode> nodeStack       = new LinkedList<TreeNode>() {{ push(head);  }};
-        Deque<Integer>  leftIndexStack  = new LinkedList<Integer>()  {{ push(0);     }};
-        Deque<Integer>  rightIndexStack = new LinkedList<Integer>()  {{ push(len-1); }};
-        
-        while ( !nodeStack.isEmpty() ) {
-            TreeNode currNode = nodeStack.pop();
-            int left  = leftIndexStack.pop();
-            int right = rightIndexStack.pop();
-            int mid   = left + (right-left)/2; // avoid overflow
-            currNode.val = nums[mid];
-            if ( left <= mid-1 ) {
-                currNode.left = new TreeNode(0);  
-                nodeStack.push(currNode.left);
-                leftIndexStack.push(left);
-                rightIndexStack.push(mid-1);
-            }
-            if ( mid+1 <= right ) {
-                currNode.right = new TreeNode(0);
-                nodeStack.push(currNode.right);
-                leftIndexStack.push(mid+1);
-                rightIndexStack.push(right);
-            }
-        }
-        return head;
-    }
+  public TreeNode sortedArrayToBST(int[] nums) {
+    return helper(nums, 0, nums.length-1);
+  }
 
+  private TreeNode helper(int[] nums, int l, int r){
+    if (l>r) return null;
+    if (l==r) return new TreeNode(nums[l]);
+    int mid = (l+r)/2;
+    TreeNode root = new TreeNode(nums[mid]);
+    root.left = helper(nums, l, mid-1);
+    root.right = helper(nums, mid+1, r);
+    return root;
+  }
 }
