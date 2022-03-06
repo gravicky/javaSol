5fcdeb13eacf3e931aafb811f6c674a0695eafdc
gravicky <100748949+gravicky@users.noreply.github.com>
2022-03-06 14:59:00 +0530

    Time: 5 ms (68.96%), Space: 43.6 MB (42.83%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -1,10 +1,17 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        char[] ch = s.toCharArray();
-        char[] chr=t.toCharArray();
-        Arrays.sort(ch);
-        Arrays.sort(chr);
-        return Arrays.equals(ch,chr);
+        int[] alp=new int[26];
+        for(int i=0;i<s.length();i++)
+            alp[s.charAt(i) - 'a']++;
+        for(int i=0;i<t.length();i++){
+            alp[t.charAt(i) - 'a']--;
+            if(alp[t.charAt(i)-'a']<0)
+                return false;
+        }
+        for(int i=0;i<26;i++)
+            if(alp[i]!=0)
+                return false;
+        return true;
         
         
     }
