04a061ade6c3a632821719e8e5cb38354c78b610
gravicky <100748949+gravicky@users.noreply.github.com>
2022-03-07 10:11:13 +0530

    Time: 1 ms (99.41%), Space: 40.7 MB (85.20%) - LeetHub

--- 171-excel-sheet-column-number/171-excel-sheet-column-number.java
+++ 171-excel-sheet-column-number/171-excel-sheet-column-number.java
@@ -3,7 +3,7 @@ class Solution {
         int n=columnTitle.length()-1;
         int sum=0;
         for(int i=0;i<=n;i++){
-            sum+=(columnTitle.charAt(i)-'A'+1)*Math.pow(26,n-i);
+            sum=sum*26+(columnTitle.charAt(i)-'A'+1);
         }
         return sum;
         
