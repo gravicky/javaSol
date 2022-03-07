daf293f8f4f4b7b5eabb1b963d725ef15cf2133a
gravicky <100748949+gravicky@users.noreply.github.com>
2022-03-07 14:32:15 +0530

    Time: 0 ms (100.00%), Space: 42.1 MB (57.84%) - LeetHub

--- 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.java
+++ 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.java
@@ -15,24 +15,19 @@
  */
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
-        Map<Integer,ArrayList<Integer>> map=new HashMap<>();
-        backtrack(map,root,0);
         List<List<Integer>> list=new ArrayList<>();
-        for (Integer key: map.keySet()){  
-			list.add(map.get(key));
-        }
+        backtrack(list,root,0);
         return list;
     }
     
-    public void backtrack( Map<Integer,ArrayList<Integer>> map,TreeNode root, int depth){
-        if(root!=null){
-            ArrayList<Integer> lis=map.getOrDefault(depth,new ArrayList<>()); 
-            lis.add(root.val); 
-            map.put(depth,lis);
-            backtrack(map,root.left,depth+1);
-            backtrack(map,root.right,depth+1);
-        }
-        return;
+    public void backtrack( List<List<Integer>> list,TreeNode root, int depth){
+        if(root==null)
+            return;
+        if(depth>=list.size())
+            list.add(new ArrayList<>());
+            list.get(depth).add(root.val);
+            backtrack(list,root.left,depth+1);
+            backtrack(list,root.right,depth+1);
         
     }
 }
