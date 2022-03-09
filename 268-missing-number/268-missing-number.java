28491d5d33feb684cf10db26b48179cabda99175
gravicky <100748949+gravicky@users.noreply.github.com>
2022-03-09 21:42:54 +0530

    Time: 0 ms (100.00%), Space: 51.5 MB (22.74%) - LeetHub

--- 268-missing-number/268-missing-number.java
+++ 268-missing-number/268-missing-number.java
@@ -1,24 +1,12 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        HashSet map=new HashSet<>(nums.length);
-        if(nums==null)
-            return 0;
-        int max=nums[0],min=nums[0];
-        for(int i=0;i<nums.length;i++){
-            map.add(nums[i]);
-            min=Math.min(min,nums[i]);
-            max=Math.max(max,nums[i]);
+        int n=nums.length,sum=0;
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+            
         }
-        int k=min+1;
-        while(k<max){
-            if(!map.contains(k))
-                return k;
-            k++;
-        }
+        int fin=(n)*(n+1)/2-sum;
+        return fin;
         
-        if(min==0)
-            return (max+1);
-        else
-            return (min-1);
-}
+    }
 }
