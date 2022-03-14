83842b0a02b7ddc990fd1f2552e43dec7ca06521
gravicky <100748949+gravicky@users.noreply.github.com>
2022-03-14 22:36:27 +0530

    Time: 8 ms (29.58%), Space: 43.1 MB (21.12%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
@@ -1,17 +1,25 @@
-class Solution {
-    public List<String> letterCombinations(String digits) {
-		LinkedList<String> ans = new LinkedList<String>();
-		if(digits.isEmpty()) return ans;
-		String[] mapping = new String[] {"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
-		ans.add("");
-		for(int i =0; i<digits.length();i++){
-			int x = Character.getNumericValue(digits.charAt(i));
-			while(ans.peek().length()==i){
-				String t = ans.remove();
-				for(char s : mapping[x].toCharArray())
-					ans.add(t+s);
-			}
-		}
-		return ans;
-	}
-}
+
+    public class Solution {
+        public static List<String> letterCombinations(String digits) {
+            String digitletter[] = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+            List<String> result = new ArrayList<String>();
+    
+            if (digits.length()==0) return result;
+            
+            result.add("");
+            for (int i=0; i<digits.length(); i++) 
+                result = combine(digitletter[digits.charAt(i)-'0'],result);
+            
+            return result;
+        }
+        
+        public static List<String> combine(String digit, List<String> l) {
+            List<String> result = new ArrayList<String>();
+            
+            for (int i=0; i<digit.length(); i++) 
+                for (String x : l) 
+                    result.add(x+digit.charAt(i));
+    
+            return result;
+        }
+    }
