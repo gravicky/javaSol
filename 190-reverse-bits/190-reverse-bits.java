de42fdbe34b59844b49f0d604457bf96e5977376
gravicky <100748949+gravicky@users.noreply.github.com>
2022-03-21 21:57:12 +0530

    Time: 1 ms (84.70%), Space: 42.1 MB (60.26%) - LeetHub

--- 190-reverse-bits/190-reverse-bits.java
+++ 190-reverse-bits/190-reverse-bits.java
@@ -5,9 +5,9 @@ public class Solution {
             return 0;
         int result=0;
         for(int i=0;i<32;i++){
+            int end=n&1;
             result<<=1;
-            if((n&1) == 1)
-                result|=1;
+            result|=end;
             n>>=1;
         }
         return result;
