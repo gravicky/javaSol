e062051b3848d307db4d184de4bfe8c85b80e28c
gravicky <100748949+gravicky@users.noreply.github.com>
2022-03-22 22:31:31 +0530

    Time: 2 ms (93.56%), Space: 42 MB (59.79%) - LeetHub

--- 38-count-and-say/38-count-and-say.java
+++ 38-count-and-say/38-count-and-say.java
@@ -1,23 +1,30 @@
-class Solution {
+public class Solution {
     public String countAndSay(int n) {
-        String s="1";
-        int i=2;
-        while(i<=n){
-            String temp="";
-            int j=0,len=s.length();
-            while(j<len){
-                int count=1;
-                while((j<len-1) && s.charAt(j)==s.charAt(j+1)){
-                    count++;
-                    j++;
-                }
-                temp=temp+count+s.charAt(j);
-                j++;
-            }
-            s=new String(temp);
-            i++;
+        String s = "1";
+        for(int i = 1; i < n; i++){
+            s = countIdx(s);
         }
         return s;
-        
+    }
+    
+    public String countIdx(String s){
+        StringBuilder sb = new StringBuilder();
+        char c = s.charAt(0);
+        int count = 1;
+        for(int i = 1; i < s.length(); i++){
+            if(s.charAt(i) == c){
+                count++;
+            }
+            else
+            {
+                sb.append(count);
+                sb.append(c);
+                c = s.charAt(i);
+                count = 1;
+            }
+        }
+        sb.append(count);
+        sb.append(c);
+        return sb.toString();
     }
 }
