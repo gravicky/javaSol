86dac3a3f8a25513f5f3864bcd6f927782794f81
gravicky <100748949+gravicky@users.noreply.github.com>
2022-03-24 23:27:39 +0530

    Time: 0 ms (100.00%), Space: 45.6 MB (41.68%) - LeetHub

--- 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.java
+++ 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.java
@@ -14,20 +14,24 @@ class Node {
 */
 
 class Solution {
-    public Node copyRandomList(Node head) {
-        Map<Node,Node> map = new HashMap<>();
-        for(Node n=head;n!=null;n=n.next){
-            map.put(n,new Node(n.val));
-        }
-        
-        for(Node n=head;n!=null;n=n.next){
-            map.get(n).next = map.get(n.next);
-            map.get(n).random = map.get(n.random);
-        }
-        
-        return map.get(head);
-        
-        
-        
+public Node copyRandomList(Node head) {
+	//<original, clone>
+    HashMap<Node, Node> maps = new HashMap<>();
+    Node cursor = head;
+    while(null != cursor){
+        maps.put(cursor, new Node(cursor.val));
+        cursor = cursor.next; 
     }
+    cursor = head;
+    while(null != cursor){
+    	maps.get(cursor).random = maps.get(cursor.random);
+    	cursor = cursor.next;
+    }
+    cursor = head;
+    while(null != cursor){
+    	maps.get(cursor).next = maps.get(cursor.next);
+        cursor = cursor.next;
+    }
+    return maps.get(head);
+}
 }
