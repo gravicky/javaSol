85301caabe9a3b9cd5d9d423abba8e7a0df6c0e7
gravicky <100748949+gravicky@users.noreply.github.com>
2022-05-03 16:45:20 +0530

    Time: 49 ms (5.52%), Space: 40.8 MB (46.94%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.java
+++ 278-first-bad-version/278-first-bad-version.java
@@ -3,11 +3,7 @@
 
 public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
-        if(isBadVersion(n) && !isBadVersion(n-1))
-            return n;
-        if(isBadVersion(1))
-            return 1;
-        int low = 1,high = n-1,mid;
+        int low = 0,high = n,mid;
         while(high>=low){
             mid = low + (high-low)/2;
             if(!isBadVersion(mid) && isBadVersion(mid+1))
