027a606279b4af968a99167fc73872dcc232a811
gravicky <100748949+gravicky@users.noreply.github.com>
2022-05-03 16:46:53 +0530

    Time: 15 ms (77.02%), Space: 40.7 MB (53.53%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.java
+++ 278-first-bad-version/278-first-bad-version.java
@@ -1,18 +1,15 @@
-/* The isBadVersion API is defined in the parent class VersionControl.
-      boolean isBadVersion(int version); */
-
 public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
-        int low = 0,high = n,mid;
-        while(high>=low){
-            mid = low + (high-low)/2;
-            if(!isBadVersion(mid) && isBadVersion(mid+1))
-                return mid+1;
-            if(!isBadVersion(mid))
-                low = mid+1;
-            else
-                high = mid-1;
+        int left = 1, right = n, ans = -1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2; // to avoid overflow incase (left+right)>2147483647
+            if (isBadVersion(mid)) {
+                ans = mid; // record mid as current answer
+                right = mid - 1; // try to find smaller version in the left side
+            } else {
+                left = mid + 1; // try to find in the right side
+            }
         }
-     return 0;  
+        return ans;
     }
 }
