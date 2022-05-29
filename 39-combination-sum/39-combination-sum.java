59a4f80729041de68da74ab21bdbe64481e8ad4c
gravicky <100748949+gravicky@users.noreply.github.com>
2022-05-29 20:52:57 +0530

    Time: 6 ms (52.28%), Space: 44.4 MB (67.22%) - LeetHub

--- 39-combination-sum/39-combination-sum.java
+++ 39-combination-sum/39-combination-sum.java
@@ -1,20 +1,23 @@
 class Solution {
-public List<List<Integer>> combinationSum(int[] nums, int target) {
-    List<List<Integer>> list = new ArrayList<>();
-    Arrays.sort(nums);
-    backtrack(list, new ArrayList<>(), nums, target, 0);
-    return list;
-}
+ public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+        backtrack(result, candidates, target, new ArrayList<>(), 0);
+        return result;
+    }
 
-private void backtrack(List<List<Integer>> list, List<Integer> tempList, int [] nums, int remain, int start){
-    if(remain < 0) return;
-    else if(remain == 0) list.add(new ArrayList<>(tempList));
-    else{ 
-        for(int i = start; i < nums.length; i++){
-            tempList.add(nums[i]);
-            backtrack(list, tempList, nums, remain - nums[i], i); // not i + 1 because we can reuse same elements
-            tempList.remove(tempList.size() - 1);
+    private void backtrack(List<List<Integer>> result, int[] candidates, int target, List<Integer> prefix, int startAt) {
+        if (target < 0) {
+            return;
+        }
+        if (target == 0) {
+            result.add(new ArrayList<>(prefix));
+        } else {
+            for (int i = startAt; i < candidates.length; i++) {
+                prefix.add(candidates[i]);
+                // todo bug: do not start at i + 1, because we can reuse same elements
+                backtrack(result, candidates, target - candidates[i], prefix, i);
+                prefix.remove(prefix.size() - 1);
+            }
         }
     }
-}
 }
