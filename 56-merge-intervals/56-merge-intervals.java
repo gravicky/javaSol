b64d91c090e9915ef8690ea07914c05bc98620c9
gravicky <100748949+gravicky@users.noreply.github.com>
2022-05-30 22:41:40 +0530

    Time: 8 ms (88.62%), Space: 55.6 MB (17.67%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.java
+++ 56-merge-intervals/56-merge-intervals.java
@@ -1,27 +1,29 @@
 
 	class Solution {
 	public int[][] merge(int[][] intervals) {
-		if (intervals.length <= 1)
-			return intervals;
-		Arrays.sort(intervals, (i1, i2) -> Integer.compare(i1[0],i2[0]));
-
-		List<int[]> result = new ArrayList<>();
-		int[] oldInterval = intervals[0];
-        int m=intervals.length,i=1;
-		while(i<m) 
-        {
-			if (intervals[i][0] <= oldInterval[1]){
-				oldInterval[1] = Math.max(oldInterval[1], intervals[i][1]);
-                i++;
+        int length=intervals.length;
+        if(length<=1)
+            return intervals;
+    
+        int[] start = new int[length];
+        int[] end = new int[length];
+        for(int i=0;i<length;i++){
+            start[i]=intervals[i][0];
+            end[i]=intervals[i][1];
+        }
+        Arrays.sort(start);
+        Arrays.sort(end);
+        int startIndex=0;
+        int endIndex=0;
+        List<int[]> result = new LinkedList<>();
+        while(endIndex<length){
+            //as endIndex==length-1 is evaluated first, start[endIndex+1] will never hit out of index
+            if(endIndex==length-1 || start[endIndex+1]>end[endIndex]){
+                result.add(new int[]{start[startIndex],end[endIndex]});
+                startIndex=endIndex+1;
             }
-			else {                             // Disjoint intervals, add the new interval to the list
-				result.add(oldInterval);
-                oldInterval = intervals[i];
-                i++;				
-			}
-		}
-        result.add(oldInterval);
-
-		return result.toArray(new int[result.size()][]);
-	}
+            endIndex++;
+        }
+        return result.toArray(new int[result.size()][]);
+    }
 }
