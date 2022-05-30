3dc138589154c1ca26d0364b6d25328cb4b9ee44
gravicky <100748949+gravicky@users.noreply.github.com>
2022-05-30 22:20:48 +0530

    Time: 14 ms (41.04%), Space: 55.4 MB (38.39%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.java
+++ 56-merge-intervals/56-merge-intervals.java
@@ -5,10 +5,9 @@ class Solution {
 
 		// Sort by ascending starting point
 		Arrays.sort(intervals, (i1, i2) -> Integer.compare(i1[0], i2[0]));
-
 		List<int[]> result = new ArrayList<>();
 		int[] newInterval = intervals[0];
-		result.add(newInterval);
+        result.add(newInterval);
 		for (int[] interval : intervals) {
 			if (interval[0] <= newInterval[1]) // Overlapping intervals, move the end if needed
 				newInterval[1] = Math.max(newInterval[1], interval[1]);
