e6878137c1b461cca2281325c009507181afe4cb
gravicky <100748949+gravicky@users.noreply.github.com>
2022-07-20 22:12:08 +0530

    Time: 3 ms (45.01%), Space: 44.1 MB (30.89%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.java
+++ 22-generate-parentheses/22-generate-parentheses.java
@@ -1,20 +1,22 @@
 class Solution {
-    public List<String> generateParenthesis(int n) {
-        List<String> res = new  ArrayList<>();
-        if(n==0)
-            return res;
-        brackets(res, "",n,n);
-        return res;
-        
+   public List<String> generateParenthesis(int n) {
+    List<String> list = new ArrayList<String>();
+    generateOneByOne("", list, n, n);
+    return list;
+}
+public void generateOneByOne(String sublist, List<String> list, int left, int right){
+    if(left > right){
+        return;
     }
-    public void brackets(List<String> res, String fix, int left, int right){
-        if(left==0 && right==0)
-            res.add(fix);
-        if(left>0){
-            brackets(res,fix+"(",left-1,right);
-        }
-        if(right>left){
-             brackets(res,fix+")",left,right-1);
-        }
+    if(left > 0){
+        generateOneByOne( sublist + "(" , list, left-1, right);
     }
+    if(right > 0){
+        generateOneByOne( sublist + ")" , list, left, right-1);
+    }
+    if(left == 0 && right == 0){
+        list.add(sublist);
+        return;
+    }
+}
 }
