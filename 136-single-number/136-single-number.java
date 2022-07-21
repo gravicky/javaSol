05f1c35c553863e58dd0bdde6b927dec7a4377c7
gravicky <100748949+gravicky@users.noreply.github.com>
2022-07-21 21:21:48 +0530

    Time: 2 ms (63.68%), Space: 49.7 MB (81.45%) - LeetHub

--- 136-single-number/136-single-number.java
+++ 136-single-number/136-single-number.java
@@ -1,15 +1,8 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        Map<Integer,Integer> map=new HashMap<>();
-        for(int i=0;i<nums.length;i++){
-            if(map.containsKey(nums[i]))
-                map.put(nums[i],2);
-            else
-                map.put(nums[i],1);
-        }
+        int res = 0;
         for(int i=0;i<nums.length;i++)
-            if(map.get(nums[i])==1)
-                return nums[i];
-    return -1;    
+            res ^=nums[i];
+        return res;
     }
 }
