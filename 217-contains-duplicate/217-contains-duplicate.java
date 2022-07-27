4ffca2ff77a0f556bd2fe04fa8fda637f2b4406f
gravicky <100748949+gravicky@users.noreply.github.com>
2022-07-27 19:57:04 +0530

    Time: 10 ms (76.26%), Space: 68.1 MB (76.25%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.java
+++ 217-contains-duplicate/217-contains-duplicate.java
@@ -1,16 +1,12 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        if(nums==null)
-            throw new IllegalArgumentException("Input array is null");
-        if(nums.length==1)
+        if(nums.length == 1)
             return false;
-        HashSet<Integer> set=new HashSet<>();
+        HashSet<Integer> set = new HashSet<>();
         for(int i:nums){
             if(!set.add(i))
                 return true;
-            
         }
         return false;
-        
     }
 }
