3de6e8c9f6638beb11265f0df6d2da0597151c4b
gravicky <100748949+gravicky@users.noreply.github.com>
2022-07-30 20:58:22 +0530

    Time: 2 ms (82.75%), Space: 55 MB (27.83%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.java
+++ 283-move-zeroes/283-move-zeroes.java
@@ -1,15 +1,16 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int pos=0;
+        int snowball = 0;
         for(int i=0;i<nums.length;i++){
-            if(nums[i]!=0){
-                if(i!=pos){
-                    nums[pos]=nums[i];
-                    nums[i]=0;
-            
+            if(nums[i]==0)
+                snowball++;
+            else
+                if(snowball>0){
+                    int t = nums[i];
+                    nums[i-snowball] = t;
+                    nums[i] = 0;
                 }
-                pos++;
-            }
         }
+        
     }
 }
