fa22088ec9ed8f29a0ce99555f8e7e271591ecd0
gravicky <100748949+gravicky@users.noreply.github.com>
2022-08-14 20:54:32 +0530

    Time: 16 ms (71.93%), Space: 80.1 MB (20.60%) - LeetHub

--- 148-sort-list/148-sort-list.java
+++ 148-sort-list/148-sort-list.java
@@ -8,53 +8,40 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-
-    public class Solution {
-  
-  public ListNode sortList(ListNode head) {
-    if (head == null || head.next == null)
-      return head;
+class Solution {
+    public ListNode sortList(ListNode head) {
+        if(head == null || head.next == null)
+            return head;
+        ListNode prev = null, slow = head, fast = head;
+        while(fast!=null && fast.next!=null){
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        prev.next = null;
+        ListNode l1 =  sortList(head);
+        ListNode l2 = sortList(slow);
+        return merge(l1,l2);
         
-    // step 1. cut the list to two halves
-    ListNode prev = null, slow = head, fast = head;
-    
-    while (fast != null && fast.next != null) {
-      prev = slow;
-      slow = slow.next;
-      fast = fast.next.next;
     }
+    ListNode merge(ListNode l1, ListNode l2){
+        ListNode res = new ListNode(0), s = res;
+        while(l1!=null && l2 !=null){
+            if(l1.val>l2.val){
+                s.next = l2;
+                l2 = l2.next;
+            }
+            else{
+                s.next = l1;
+                l1 = l1.next;
+            }
+            s = s.next;
+        }
+        if (l1 != null)
+            s.next = l1;
     
-    prev.next = null;
-    
-    // step 2. sort each half
-    ListNode l1 = sortList(head);
-    ListNode l2 = sortList(slow);
-    
-    // step 3. merge l1 and l2
-    return merge(l1, l2);
-  }
-  
-  ListNode merge(ListNode l1, ListNode l2) {
-    ListNode l = new ListNode(0), p = l;
-    
-    while (l1 != null && l2 != null) {
-      if (l1.val < l2.val) {
-        p.next = l1;
-        l1 = l1.next;
-      } else {
-        p.next = l2;
-        l2 = l2.next;
-      }
-      p = p.next;
+         if (l2 != null)
+            s.next = l2;
+        return res.next;
     }
-    
-    if (l1 != null)
-      p.next = l1;
-    
-    if (l2 != null)
-      p.next = l2;
-    
-    return l.next;
-  }
-
 }
