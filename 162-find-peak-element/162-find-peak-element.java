fa352dc6945b592f67ea94053a2d02e18ad06b3b
gravicky <100748949+gravicky@users.noreply.github.com>
2022-08-26 21:14:03 +0530

    Time: 0 ms (100.00%), Space: 42.6 MB (70.76%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.java
+++ 162-find-peak-element/162-find-peak-element.java
@@ -1,27 +1,22 @@
 class Solution {
     public int findPeakElement(int[] nums) {
-        return find(nums,0,nums.length-1);
+       return findpeak(0,nums.length-1,nums);
     }
-    
-    public int find(int[] nums, int start, int end){
-        if(start==end)
-            return start;
+    public int findpeak(int st,int end,int[]nums){
+        if(st==end)
+            return st;
+        if(st==end-1)
+            if(nums[st]>nums[end])
+                return st;
+            else
+                return end;
+        int mid = (end-st)/2 + st;
+        if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1])
+            return mid;
         else
-            if(start==end-1){
-                if(nums[start]>nums[end])
-                    return start;
-                else
-                    return end;
-            }
-        else{
-            int m = (end-start)/2 + start;
-            if(nums[m]>nums[m-1] && nums[m]>nums[m+1])
-                return m;
+            if(nums[mid]<nums[mid-1] && nums[mid]>nums[mid+1])
+                return findpeak(0,mid-1,nums);
             else
-                if(nums[m-1]>nums[m] && nums[m]>nums[m+1])
-                    return find(nums,start,m-1);
-            else
-                return find(nums,m+1,end);
-        }
+                return findpeak(mid+1,end,nums);
     }
 }
